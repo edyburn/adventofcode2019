cf5f6f95e9c808de7b75d6b77d2a511c3216e1fa
Keith Edyburn <keith@edyburn.info>
2019-12-03 16:29:56 -0500

    Solution for day 1 part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,11 +6,24 @@ fn calculate_fuel(mass: f32) -> f32 {
     (mass / 3.0).floor() - 2.0
 }
 
+fn calculate_module_fuel(mass: f32) -> f32 {
+    let mut total = 0.0;
+    let mut _mass = mass;
+    loop {
+        let fuel = calculate_fuel(_mass);
+        if fuel <= 0.0 {
+            return total;
+        };
+        total += fuel;
+        _mass = fuel;
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let input = fs::read_to_string("input.txt")?;
     let result: f32 = input
         .lines()
-        .map(|l| calculate_fuel(l.parse::<f32>().unwrap()))
+        .map(|l| calculate_module_fuel(l.parse::<f32>().unwrap()))
         .sum();
     println!("Total: {}", result);
     Ok(())
