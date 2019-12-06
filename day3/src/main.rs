c8003f1b6ca3cab87ac73a976ea8f0de5ac2aa95
Keith Edyburn <keith@edyburn.info>
2019-12-05 23:22:11 -0500

    Solution for day 3 part 1.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashSet;
+use std::fs;
+
+fn parse_path(path: &str) -> HashSet<(i32, i32)> {
+    let mut coord_set = HashSet::new();
+    let mut x = 0;
+    let mut y = 0;
+    for segment in path.split(',') {
+        let (direction, distance) = segment.split_at(1);
+        let distance = distance.parse::<i32>().unwrap();
+        for _ in 0..distance {
+            match direction {
+                "R" => x += 1,
+                "L" => x -= 1,
+                "U" => y += 1,
+                "D" => y -= 1,
+                _ => panic!("Invalid direction"),
+            };
+            coord_set.insert((x, y));
+        }
+    }
+    coord_set
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let input = fs::read_to_string("input.txt")?;
+    let lines: Vec<&str> = input.lines().collect();
+    let wire1_path = parse_path(lines[0]);
+    let wire2_path = parse_path(lines[1]);
+    let result = wire1_path
+        // Intersect to find where wires cross
+        .intersection(&wire2_path)
+        // Compute Manhattan distance from center
+        .map(|(x, y)| x.abs() + y.abs())
+        // Find minimum
+        .min();
+    println!("result: {:?}", result);
+    Ok(())
 }
