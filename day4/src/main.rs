ff84a109b4e1bdfe50a09548700bcffccc7d891f
Keith Edyburn <keith@edyburn.info>
2019-12-06 01:32:41 -0500

    Solution for day 4 part 2.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,12 @@
 fn has_pair(s: &[char]) -> bool {
     for i in 0..(s.len() - 1) {
         if s[i] == s[i + 1] {
+            if (i > 0) && (s[i - 1] == s[i]) {
+                continue;
+            }
+            if (i < (s.len() - 2)) && (s[i] == s[i + 2]) {
+                continue;
+            }
             return true;
         }
     }
@@ -18,7 +24,7 @@ fn is_increasing(s: &[char]) -> bool {
 
 fn validate(n: u32) -> bool {
     let s: Vec<char> = n.to_string().chars().collect();
-    // Two adjacent digits are the same
+    // Two adjacent digits are the same but not part of a larger group of matching digits
     // Going from left to right, the digits never decrease
     has_pair(&s) & is_increasing(&s)
 }
