0b541f79d5b672ca90880d72383546b928c14836
Keith Edyburn <keith@edyburn.info>
2019-12-06 23:22:16 -0500

    Solution for day 6 part 1.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
+fn count_orbits(orbits: &HashMap<&str, Vec<&str>>, root: &str, depth: u32) -> u32 {
+    let sat_count: u32 = match orbits.get(root) {
+        Some(satellites) => satellites
+            .iter()
+            .map(|satellite| count_orbits(orbits, satellite, depth + 1))
+            .sum(),
+        None => 0,
+    };
+    depth + sat_count
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt").trim();
+    let mut orbits: HashMap<&str, Vec<&str>> = HashMap::new();
+    for line in input.lines() {
+        let pieces: Vec<_> = line.split(')').collect();
+        let satellites = orbits.entry(pieces[0]).or_default();
+        satellites.push(pieces[1]);
+    }
+
+    let result = count_orbits(&orbits, "COM", 0);
+    println!("result: {}", result);
 }
