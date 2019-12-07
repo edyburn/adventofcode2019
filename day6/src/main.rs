0e54f1d003b513c95bea5a71d2d1ee8c574f9b30
Keith Edyburn <keith@edyburn.info>
2019-12-06 23:48:53 -0500

    Solution for day 6 part 2.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,25 +1,28 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
-fn count_orbits(orbits: &HashMap<&str, Vec<&str>>, root: &str, depth: u32) -> u32 {
-    let sat_count: u32 = match orbits.get(root) {
-        Some(satellites) => satellites
-            .iter()
-            .map(|satellite| count_orbits(orbits, satellite, depth + 1))
-            .sum(),
-        None => 0,
-    };
-    depth + sat_count
+fn get_path<'a>(orbits: &HashMap<&str, &'a str>, start: &'a str) -> HashSet<&'a str> {
+    let mut path = HashSet::new();
+    let mut cursor = start;
+    while cursor != "COM" {
+        cursor = orbits.get(cursor).unwrap();
+        path.insert(cursor);
+    }
+    path
 }
 
 fn main() {
     let input = include_str!("../input.txt").trim();
-    let mut orbits: HashMap<&str, Vec<&str>> = HashMap::new();
-    for line in input.lines() {
-        let pieces: Vec<_> = line.split(')').collect();
-        let satellites = orbits.entry(pieces[0]).or_default();
-        satellites.push(pieces[1]);
-    }
+    let orbits: HashMap<&str, &str> = input
+        .lines()
+        .map(|line| {
+            let pieces: Vec<_> = line.split(')').collect();
+            (pieces[1], pieces[0])
+        })
+        .collect();
 
-    let result = count_orbits(&orbits, "COM", 0);
+    let you_path = get_path(&orbits, "YOU");
+    let san_path = get_path(&orbits, "SAN");
+
+    let result = you_path.symmetric_difference(&san_path).count();
     println!("result: {}", result);
 }
