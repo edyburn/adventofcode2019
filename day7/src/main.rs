6437a0a9452942f1165919e07039b7918e2f33f1
Keith Edyburn <keith@edyburn.info>
2019-12-07 02:27:32 -0500

    Solution for day 7 part 2.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,100 +1,129 @@
 use permute::permute;
 
-fn memory_lookup(memory: &[isize], index: usize, mode: usize) -> isize {
-    let value = memory[index];
-    if mode == 0 {
-        memory[value as usize]
-    } else {
-        value
-    }
+struct Program {
+    memory: Vec<isize>,
+    pointer: usize,
+    input: Vec<isize>,
+    halted: bool,
 }
 
-fn run_program(mut memory: Vec<isize>, mut input: Vec<isize>) -> Vec<isize> {
-    let mut cursor: usize = 0;
-    let mut output = Vec::new();
+enum InstructionResult {
+    None,
+    InputBlocked,
+    Output(isize),
+    Halt,
+}
 
-    loop {
-        let instruction = memory[cursor];
-        let opcode = instruction % 100;
-        let params = instruction as usize / 100;
-        // println!("instruction: {}", instruction);
-        match opcode {
-            // add, step 4
-            1 => {
-                let a = memory_lookup(&memory, cursor + 1, params & 1);
-                let b = memory_lookup(&memory, cursor + 2, (params / 10) & 1);
-                let sum = a + b;
-                let result_pos = memory[cursor + 3] as usize;
-                memory[result_pos] = sum;
-                // println!("add: {}", sum);
-                cursor += 4;
-            }
-            // multiply, step 4
-            2 => {
-                let a = memory_lookup(&memory, cursor + 1, params & 1);
-                let b = memory_lookup(&memory, cursor + 2, (params / 10) & 1);
-                let product = a * b;
-                let result_pos = memory[cursor + 3] as usize;
-                memory[result_pos] = product;
-                // println!("mult: {}", product);
-                cursor += 4;
-            }
-            // input, step 2
-            3 => {
-                let input_value = input.remove(0);
-                let result_pos = memory[cursor + 1] as usize;
-                memory[result_pos] = input_value;
-                // println!("save: {}", value);
-                cursor += 2;
-            }
-            // output, step 2
-            4 => {
-                let output_value = memory_lookup(&memory, cursor + 1, params & 1);
-                output.push(output_value);
-                // println!("output: {}", output_value);
-                cursor += 2;
-            }
-            // jump-if-true
-            5 => {
-                let value = memory_lookup(&memory, cursor + 1, params & 1);
-                if value != 0 {
-                    cursor = memory_lookup(&memory, cursor + 2, (params / 10) & 1) as usize;
-                } else {
-                    cursor += 3;
-                }
-            }
-            // jump-if-false
-            6 => {
-                let value = memory_lookup(&memory, cursor + 1, params & 1);
-                if value == 0 {
-                    cursor = memory_lookup(&memory, cursor + 2, (params / 10) & 1) as usize;
-                } else {
-                    cursor += 3;
-                }
-            }
-            // less than
-            7 => {
-                let a = memory_lookup(&memory, cursor + 1, params & 1);
-                let b = memory_lookup(&memory, cursor + 2, (params / 10) & 1);
-                let result_pos = memory[cursor + 3] as usize;
-                memory[result_pos] = if a < b { 1 } else { 0 };
-                cursor += 4;
-            }
-            // equal
-            8 => {
-                let a = memory_lookup(&memory, cursor + 1, params & 1);
-                let b = memory_lookup(&memory, cursor + 2, (params / 10) & 1);
-                let result_pos = memory[cursor + 3] as usize;
-                memory[result_pos] = if a == b { 1 } else { 0 };
-                cursor += 4;
-            }
-            // halt
-            99 => break,
-            _ => panic!(format!("unknown opcode: {}", opcode)),
+impl Program {
+    fn new(memory: Vec<isize>, input: Vec<isize>) -> Program {
+        Program {
+            memory,
+            pointer: 0,
+            input,
+            halted: false,
         }
     }
 
-    output
+    fn lookup(&self, index: usize, mode: usize) -> isize {
+        let value = self.memory[index];
+        if mode == 0 {
+            self.memory[value as usize]
+        } else {
+            value
+        }
+    }
+
+    fn execute_instruction(&mut self) -> InstructionResult {
+        if self.halted {
+            return InstructionResult::Halt;
+        }
+        let instruction = self.memory[self.pointer];
+        let opcode = instruction % 100;
+        let params = instruction as usize / 100;
+        match opcode {
+            // add, step 4
+            1 => {
+                let a = self.lookup(self.pointer + 1, params & 1);
+                let b = self.lookup(self.pointer + 2, (params / 10) & 1);
+                let sum = a + b;
+                let result_pos = self.memory[self.pointer + 3] as usize;
+                self.memory[result_pos] = sum;
+                self.pointer += 4;
+                InstructionResult::None
+            }
+            // multiply, step 4
+            2 => {
+                let a = self.lookup(self.pointer + 1, params & 1);
+                let b = self.lookup(self.pointer + 2, (params / 10) & 1);
+                let product = a * b;
+                let result_pos = self.memory[self.pointer + 3] as usize;
+                self.memory[result_pos] = product;
+                self.pointer += 4;
+                InstructionResult::None
+            }
+            // input, step 2
+            3 => {
+                if self.input.is_empty() {
+                    return InstructionResult::InputBlocked;
+                }
+                let input_value = self.input.remove(0);
+                let result_pos = self.memory[self.pointer + 1] as usize;
+                self.memory[result_pos] = input_value;
+                self.pointer += 2;
+                InstructionResult::None
+            }
+            // output, step 2
+            4 => {
+                let output_value = self.lookup(self.pointer + 1, params & 1);
+                self.pointer += 2;
+                InstructionResult::Output(output_value)
+            }
+            // jump-if-true
+            5 => {
+                let value = self.lookup(self.pointer + 1, params & 1);
+                if value != 0 {
+                    self.pointer = self.lookup(self.pointer + 2, (params / 10) & 1) as usize;
+                } else {
+                    self.pointer += 3;
+                }
+                InstructionResult::None
+            }
+            // jump-if-false
+            6 => {
+                let value = self.lookup(self.pointer + 1, params & 1);
+                if value == 0 {
+                    self.pointer = self.lookup(self.pointer + 2, (params / 10) & 1) as usize;
+                } else {
+                    self.pointer += 3;
+                }
+                InstructionResult::None
+            }
+            // less than
+            7 => {
+                let a = self.lookup(self.pointer + 1, params & 1);
+                let b = self.lookup(self.pointer + 2, (params / 10) & 1);
+                let result_pos = self.memory[self.pointer + 3] as usize;
+                self.memory[result_pos] = if a < b { 1 } else { 0 };
+                self.pointer += 4;
+                InstructionResult::None
+            }
+            // equal
+            8 => {
+                let a = self.lookup(self.pointer + 1, params & 1);
+                let b = self.lookup(self.pointer + 2, (params / 10) & 1);
+                let result_pos = self.memory[self.pointer + 3] as usize;
+                self.memory[result_pos] = if a == b { 1 } else { 0 };
+                self.pointer += 4;
+                InstructionResult::None
+            }
+            // halt
+            99 => {
+                self.halted = true;
+                InstructionResult::Halt
+            }
+            _ => panic!(format!("unknown opcode: {}", opcode)),
+        }
+    }
 }
 
 fn main() {
@@ -106,14 +135,49 @@ fn main() {
         .collect();
 
     let mut max = 0;
-    for permutation in permute(vec![0, 1, 2, 3, 4]) {
-        let mut input = 0;
-        for phase in permutation {
-            let output = run_program(data.clone(), vec![phase, input]);
-            input = output[0];
+    for permutation in permute(vec![5, 6, 7, 8, 9]) {
+        let mut programs: Vec<Program> = permutation
+            .iter()
+            .map(|phase| Program::new(data.clone(), vec![*phase as isize]))
+            .collect();
+        programs[0].input.push(0);
+        let mut active = 0;
+        let mut final_output = 0;
+        loop {
+            match programs[active].execute_instruction() {
+                InstructionResult::None => continue,
+                InstructionResult::Halt => {
+                    // Break out when all halted
+                    if programs.iter().all(|p| p.halted) {
+                        break;
+                    }
+                    // Run the next program
+                    if active == 4 {
+                        active = 0;
+                    } else {
+                        active += 1;
+                    }
+                }
+                InstructionResult::InputBlocked => {
+                    // Run the next program
+                    if active == 4 {
+                        active = 0;
+                    } else {
+                        active += 1;
+                    }
+                }
+                InstructionResult::Output(value) => {
+                    if active == 4 {
+                        final_output = value;
+                        programs[0].input.push(value);
+                    } else {
+                        programs[active + 1].input.push(value);
+                    }
+                }
+            }
         }
-        if input > max {
-            max = input;
+        if final_output > max {
+            max = final_output;
         }
     }
     println!("result: {}", max);
