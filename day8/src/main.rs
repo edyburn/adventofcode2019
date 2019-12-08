c1541dd408474804d1e492a6dd6fe2b960e1898a
Keith Edyburn <keith@edyburn.info>
2019-12-08 02:23:17 -0500

    Solution for day 8 part 2.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,21 +1,30 @@
 fn main() {
     let input = include_str!("../input.txt").trim();
     // The image you received is 25 pixels wide and 6 pixels tall.
+    let width = 25;
+    let height = 6;
     let layers = input.chars().fold(vec![Vec::<u32>::new()], |mut l, c| {
-        if l.last().unwrap().len() == 25 * 6 {
+        if l.last().unwrap().len() == width * height {
             l.push(Vec::new());
         }
         l.last_mut().unwrap().push(c.to_digit(10).unwrap());
         l
     });
-    // find the layer that contains the fewest 0 digits.
-    let layer = layers
-        .iter()
-        .min_by_key(|l| l.iter().filter(|&d| *d == 0).count())
-        .unwrap();
-    // On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
-    let ones = layer.iter().filter(|&d| *d == 1).count();
-    let twos = layer.iter().filter(|&d| *d == 2).count();
-    let result = ones * twos;
-    println!("result: {}", result);
+    // 0 is black, 1 is white, and 2 is transparent
+    let flattened: String = (0..layers[0].len())
+        .map(|i| {
+            for layer in layers.iter() {
+                match layer[i] {
+                    0 => return '▮',
+                    1 => return ' ',
+                    2 => continue,
+                    _ => panic!("unknown color"),
+                }
+            }
+            panic!("no color at pixel");
+        })
+        .collect();
+    for line in flattened.chars().collect::<Vec<char>>().chunks(width) {
+        println!("{}", line.iter().collect::<String>());
+    }
 }
