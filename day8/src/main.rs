18bab7dee987b336a291c43f7c61085fa7fad940
Keith Edyburn <keith@edyburn.info>
2019-12-08 01:38:05 -0500

    Solution for day 8 part 1.

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input.txt").trim();
+    // The image you received is 25 pixels wide and 6 pixels tall.
+    let layers = input.chars().fold(vec![Vec::<u32>::new()], |mut l, c| {
+        if l.last().unwrap().len() == 25 * 6 {
+            l.push(Vec::new());
+        }
+        l.last_mut().unwrap().push(c.to_digit(10).unwrap());
+        l
+    });
+    // find the layer that contains the fewest 0 digits.
+    let layer = layers
+        .iter()
+        .min_by_key(|l| l.iter().filter(|&d| *d == 0).count())
+        .unwrap();
+    // On that layer, what is the number of 1 digits multiplied by the number of 2 digits?
+    let ones = layer.iter().filter(|&d| *d == 1).count();
+    let twos = layer.iter().filter(|&d| *d == 2).count();
+    let result = ones * twos;
+    println!("result: {}", result);
 }
