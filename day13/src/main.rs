b5c44c5fa46437ec05a43c36fcde40ca4ad02121
Keith Edyburn <keith@edyburn.info>
2019-12-13 02:08:41 -0500

    Probably solution for day 13 part 2.
    Just need to beat the game...

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,16 +1,67 @@
+use crossterm::event::{read, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 use intcode::{InstructionResult, Program};
 use std::collections::HashMap;
 
+fn render(screen: &HashMap<(isize, isize), isize>) {
+    let &max_x = screen.keys().map(|(x, _)| x).max().unwrap();
+    let &min_x = screen.keys().map(|(x, _)| x).min().unwrap();
+    let &max_y = screen.keys().map(|(_, y)| y).max().unwrap();
+    let &min_y = screen.keys().map(|(_, y)| y).min().unwrap();
+    let mut buffer: Vec<Vec<char>> = (min_y..=max_y)
+        .map(|_| (min_x..=max_x).map(|_| ' ').collect())
+        .collect();
+    for ((x, y), tile_id) in screen.iter() {
+        let c = match tile_id {
+            0 => ' ',
+            1 => '█',
+            2 => '⊠',
+            3 => '=',
+            4 => '⊙',
+            _ => panic!("unknown tile id"),
+        };
+        buffer[(y - min_x) as usize][(x - min_x) as usize] = c;
+    }
+    for line in buffer.iter() {
+        println!("{}", line.iter().collect::<String>());
+    }
+}
+
 fn main() {
     let mut program = Program::from_str(include_str!("../input.txt"), Vec::new());
+    // pretend to insert quarters
+    program.memory[0] = 2;
 
     let mut screen = HashMap::new();
     let mut output_buffer = Vec::new();
+    let mut score = 0;
     loop {
         match program.execute_instruction() {
             InstructionResult::None => continue,
             InstructionResult::Halt => break,
-            InstructionResult::InputBlocked => panic!("no input connected"),
+            InstructionResult::InputBlocked => {
+                /*
+                If the joystick is in the neutral position, provide 0.
+                If the joystick is tilted to the left, provide -1.
+                If the joystick is tilted to the right, provide 1.
+                */
+                render(&screen);
+                enable_raw_mode().unwrap();
+                let input = loop {
+                    match read().unwrap() {
+                        Event::Key(event) => match event.code {
+                            KeyCode::Char('a') => break -1,
+                            KeyCode::Char('d') => break 1,
+                            KeyCode::Char('s') => break 0,
+                            _ => continue,
+                        },
+                        Event::Mouse(_) => continue,
+                        Event::Resize(_, _) => continue,
+                    }
+                };
+                disable_raw_mode().unwrap();
+                program.input.push(input);
+            }
             InstructionResult::Output(value) => {
                 /*
                 every three output instructions specify:
@@ -23,7 +74,11 @@ fn main() {
                     let tile_id = output_buffer.pop().unwrap();
                     let y = output_buffer.pop().unwrap();
                     let x = output_buffer.pop().unwrap();
-                    screen.insert((x, y), tile_id);
+                    if x == -1 && y == 0 {
+                        score = tile_id;
+                    } else {
+                        screen.insert((x, y), tile_id);
+                    }
                 }
             }
         }
@@ -36,7 +91,6 @@ fn main() {
     3 is a horizontal paddle tile. The paddle is indestructible.
     4 is a ball tile. The ball moves diagonally and bounces off objects.
     */
-    // How many block tiles are on the screen when the game exits?
-    let result = screen.values().filter(|&t| *t == 2).count();
-    println!("result: {}", result);
+    // What is your score after the last block is broken?
+    println!("result: {}", score);
 }
