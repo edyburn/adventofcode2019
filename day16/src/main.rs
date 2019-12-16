24f393558da6193075c17fd71ef20b6aa6fc271a
Keith Edyburn <keith@edyburn.info>
2019-12-16 01:27:31 -0500

    Solution for day 16 part 2.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,38 +1,30 @@
-use std::iter;
-
-const BASE_PATTERN: [isize; 4] = [0, 1, 0, -1];
-
-fn fft(signal: &[isize]) -> Vec<isize> {
+fn fft(signal: Vec<isize>) -> Vec<isize> {
     signal
         .iter()
-        .enumerate()
-        .map(|(idx, _)| {
-            let pos = idx + 1;
-            let sum: isize = signal
-                .iter()
-                .zip(
-                    BASE_PATTERN
-                        .iter()
-                        .flat_map(|x| iter::repeat(x).take(pos))
-                        .cycle()
-                        .skip(1),
-                )
-                .map(|(i, p)| i * p)
-                .sum();
-            (sum % 10).abs()
+        .scan(0, |sum, i| {
+            *sum += i;
+            Some(sum.abs() % 10)
         })
         .collect()
 }
 
 fn main() {
-    // After 100 phases of FFT, what are the first eight digits in the final output list?
     let input = include_str!("../input.txt").trim();
     let mut buffer: Vec<isize> = input
         .chars()
         .map(|c| c.to_digit(10).unwrap() as isize)
+        .cycle()
+        .take(10_000 * input.len())
         .collect();
+    let message_offset: usize = (&input[0..7]).parse().unwrap();
+    assert!(message_offset > (buffer.len() / 2));
+    let suffix = buffer.len() - message_offset;
+    buffer = buffer.into_iter().rev().take(suffix).collect();
     for _phase in 1..=100 {
-        buffer = fft(&buffer);
+        buffer = fft(buffer);
     }
-    println!("result: {:?}", &buffer[0..8]);
+    println!(
+        "result: {:?}",
+        buffer.into_iter().rev().take(8).collect::<Vec<isize>>()
+    );
 }
