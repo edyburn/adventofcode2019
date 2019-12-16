2c44b1a766821ca6b79761c7884ebc9ba0f58c95
Keith Edyburn <keith@edyburn.info>
2019-12-16 01:13:57 -0500

    Solution for day 16 part 1.

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,38 @@
+use std::iter;
+
+const BASE_PATTERN: [isize; 4] = [0, 1, 0, -1];
+
+fn fft(signal: &[isize]) -> Vec<isize> {
+    signal
+        .iter()
+        .enumerate()
+        .map(|(idx, _)| {
+            let pos = idx + 1;
+            let sum: isize = signal
+                .iter()
+                .zip(
+                    BASE_PATTERN
+                        .iter()
+                        .flat_map(|x| iter::repeat(x).take(pos))
+                        .cycle()
+                        .skip(1),
+                )
+                .map(|(i, p)| i * p)
+                .sum();
+            (sum % 10).abs()
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello, world!");
+    // After 100 phases of FFT, what are the first eight digits in the final output list?
+    let input = include_str!("../input.txt").trim();
+    let mut buffer: Vec<isize> = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as isize)
+        .collect();
+    for _phase in 1..=100 {
+        buffer = fft(&buffer);
+    }
+    println!("result: {:?}", &buffer[0..8]);
 }
