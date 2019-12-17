204285d42fe72edd09f2c7ebbdc88ccb61269d98
Keith Edyburn <keith@edyburn.info>
2019-12-17 02:22:22 -0500

    Solution for day 17 part 2.

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,43 +1,41 @@
 use intcode::{InstructionResult, Program};
 
+fn ascii_to_mem(s: &str) -> Vec<isize> {
+    s.chars().map(|c| c as isize).collect()
+}
+
 fn main() {
-    let mut program = Program::from_str(include_str!("../input.txt"), Vec::new());
+    let mut input = Vec::new();
+    // first, main movement routine
+    // - movement functions A, B, C
+    input.extend(ascii_to_mem("A,B,A,C,A,B,A,C,B,C\n"));
+    // then, each movement function
+    // - L to turn left, R to turn right, or a number to move forward)
+    input.extend(ascii_to_mem("R,4,L,12,L,8,R,4\n"));
+    input.extend(ascii_to_mem("L,8,R,10,R,10,R,6\n"));
+    input.extend(ascii_to_mem("R,4,R,10,L,12\n"));
+    // finally, want to see a continuous video feed
+    input.extend(ascii_to_mem("n\n"));
+    let mut program = Program::from_str(include_str!("../input.txt"), input);
+    program.memory[0] = 2;
     let mut buffer = String::new();
+    let mut dust = 0;
     loop {
         match program.execute_instruction() {
             InstructionResult::None => continue,
             InstructionResult::Halt => break,
             InstructionResult::InputBlocked => unimplemented!(),
             InstructionResult::Output(value) => {
-                buffer.push(value as u8 as char);
-            }
-        }
-    }
-    let lines: Vec<Vec<char>> = buffer.split('\n').map(|l| l.chars().collect()).collect();
-    let mut intersections = Vec::<(usize, usize)>::new();
-    for (y, line) in lines.iter().enumerate() {
-        if y == 0 || y == lines.len() - 1 {
-            continue;
-        }
-        for (x, &c) in line.iter().enumerate() {
-            if c != '#' || x == 0 || x == line.len() - 1 {
-                continue;
-            }
-            if line[x - 1] == '#'
-                && line[x + 1] == '#'
-                && lines[y - 1][x] == '#'
-                && lines[y + 1][x] == '#'
-            {
-                intersections.push((x, y));
+                if value > 127 {
+                    dust = value;
+                } else {
+                    buffer.push(value as u8 as char);
+                }
             }
         }
     }
 
-    // its alignment parameter is the distance between its left edge and the
-    // left edge of the view multiplied by the distance between its top edge
-    // and the top edge of the view
-
-    // What is the sum of the alignment parameters for the scaffold intersections?
-    let result: usize = intersections.iter().map(|(x, y)| x * y).sum();
-    println!("result: {}", result);
+    // println!("{}", buffer);
+    // how much dust does the vacuum robot report it has collected?
+    println!("dust: {}", dust);
 }
