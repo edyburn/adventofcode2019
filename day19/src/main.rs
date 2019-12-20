2b2097bd68a191033419343f212dd891b98a16bc
Keith Edyburn <keith@edyburn.info>
2019-12-19 22:44:51 -0500

    Solution for day 19 part 2.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,10 +1,17 @@
 use intcode::{InstructionResult, Program};
-fn main() {
-    let mut output = Vec::new();
+use std::collections::HashMap;
 
-    for x in 0..50 {
-        for y in 0..50 {
+const SIZE: isize = 100;
+fn run() -> (isize, isize) {
+    let mut last_per_col = HashMap::new();
+    let mut x = SIZE;
+    let mut min_y = 0;
+    loop {
+        let mut y = min_y;
+        let mut last = -1;
+        loop {
             let mut program = Program::from_str(include_str!("../input.txt"), vec![x, y]);
+            let mut output = 0;
             loop {
                 match program.execute_instruction() {
                     InstructionResult::None => continue,
@@ -13,14 +20,38 @@ fn main() {
                     InstructionResult::InputBlocked => unimplemented!(),
                     // Output: stationary (0) or being pulled by something (1)
                     InstructionResult::Output(value) => {
-                        output.push(value);
+                        output = value;
                     }
                 }
             }
+            if output == 1 {
+                if last == -1 {
+                    min_y = y;
+                }
+                last = y;
+                let other_x = x - (SIZE - 1);
+                if let Some(other_last) = last_per_col.get(&other_x) {
+                    if other_last - y == (SIZE - 1) {
+                        return (other_x, y);
+                    }
+                }
+            } else if last != -1 {
+                last_per_col.insert(x, last);
+                break;
+            }
+            y += 1;
         }
+        x += 1;
     }
+}
+fn main() {
+    let (x, y) = run();
 
-    // How many points are affected by the tractor beam in the 50x50 area
-    // closest to the emitter?
-    println!("{}", output.iter().filter(|&a| *a == 1).count());
+    /*
+    Find the 100x100 square closest to the emitter that fits entirely within
+    the tractor beam; within that square, find the point closest to the
+    emitter. What value do you get if you take that point's X coordinate,
+    multiply it by 10000, then add the point's Y coordinate?
+    */
+    println!("result: {}", x * 10000 + y);
 }
